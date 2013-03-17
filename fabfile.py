cc6f7482fc60be56a31ec480c20ebcecf1175fa0
flaviozantut <flaviozantut@gmail.com>
2013-03-16 22:28:54 -0300

    zip git repo to upload

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,8 @@ from fabtools.require import nginx, deb, python, files
 from fabric.contrib.console import confirm
 import fabtools
 import os
-
+import uuid
+from fabric.colors import red, green
 env.hosts = ['vagrant@localhost:2222']
 env.passwords = {'vagrant@localhost:2222': 'vagrant'}
 remote_dir = '/home/vagrant/project2/'
@@ -21,20 +22,24 @@ def  _deb_install_deps():
         'nginx',
         'python-pip',
         'python-virtualenv'
-    ], update=True)
+    ])
 
 
 def  _upload():
      # proj dir create
-    if is_dir(remote_dir) and confirm("proj dir exist! abort ?", default=False):
-        return
+    # if is_dir(remote_dir) and confirm("proj dir exist! abort ?", default=False):
+    #     return
 
     files.directory(remote_dir, use_sudo=True, owner=env.user, group=env.user, mode='755')
 
+    filename = str(uuid.uuid4())
+    local('git archive HEAD | gzip > /tmp/' + filename)
     upload_project(
-        local_dir=os.getcwd() + '/*',
-        remote_dir=remote_dir
+        local_dir='/tmp/' + filename,
+        remote_dir='/tmp/'
     )
+    run('tar zxvf /tmp/'+ filename+ ' -C '+ remote_dir)
+    local('rm /tmp/' + filename)
 
 
 def _virtualenv():
@@ -65,13 +70,19 @@ def _nginx_proxied_site():
 
 @task
 def setup():
-
+    print(green("Install deb deps"))
     _deb_install_deps()
+    print(green("upload project"))
     _upload()
+    print(green("Virtual env"))
     _virtualenv()
+    print(green("install requirements"))
     _install_requirements()
+    print(green("supervisor"))
     _supervisor()
+    print(green("nginx proxied site"))
     _nginx_proxied_site()
+    print(green("Ok"))
 
 
 
